144801e6fec4a3f581a6d2e5bef08f065d35b537
Sofia <sramirezmart@student.unimelb.edu.au>
2025-05-22 17:11:18 +1000

    Used the singleton pattern for Meld class to create a single point of access for the list of all possible melds. Delegating comparison and scoring responsibilities to a Scoring class.

--- app/src/main/java/Meld.java
+++ app/src/main/java/Meld.java
@@ -5,111 +5,64 @@ import java.util.Arrays;
 import java.util.List;
 
 /*
-    Class that contains all the different melds.
+    Class that contains all the possible melds.
     In charge of instantiating the different meld hands as Lists.
  */
-public class Meld {
-    // Baic meld.
-    MeldInterface base = new BasicMeld();
-    
-    // Ace Run based melds.
-    MeldInterface aceRun = new AceRunDecorator(new BasicMeld());
-    MeldInterface aceRunK = new KingDecorator(new AceRunDecorator(new BasicMeld()));
-    MeldInterface aceRunQ = new QueenDecorator(new AceRunDecorator(new BasicMeld()));
-    MeldInterface aceRunRM = new KingDecorator(new QueenDecorator(new AceRunDecorator(new BasicMeld())));
-    MeldInterface doubleAR = new DoubleDecorator(new AceRunDecorator(new BasicMeld()));
+public final class Meld {
+    private static Meld instance;
+    public List<MeldDecorator> melds;
 
-    // Marriage based melds.
-
-
-    // Pinochle based melds.
-    MeldInterface pinochle = new PinochleDecorator(new BasicMeld());
-    MeldInterface doubleP = new DoubleDecorator(new PinochleDecorator(new BasicMeld()));
-
-    // Jacks Abound meld.
-    MeldInterface jacksAbound = new JacksAboundDecorator(new BasicMeld());
-
-    // Aces Around meld.
-    MeldInterface acesAround = new AcesAroundDecorator(new BasicMeld());
-
-    // Dix meld.
-    MeldInterface dix = new DixDecorator(new BasicMeld());
-
-
-
-    /*
-        Create Ace Run Concrete object.
-     */
-    private List<String> getTenToAceCards() {
-        return Arrays.asList(
-                Rank.ACE.getRankCardValue() + Pinochle.trumpSuit,
-                Rank.JACK.getRankCardValue() + Pinochle.trumpSuit,
-                Rank.QUEEN.getRankCardValue() + Pinochle.trumpSuit,
-                Rank.KING.getRankCardValue() + Pinochle.trumpSuit,
-                Rank.TEN.getRankCardValue() + Pinochle.trumpSuit);
+    private Meld() {
+        populateMeld();
     }
 
-    private List<String> checkTenToAceRun(List<Card> list) {
-        List<String> cardsToCheck = getTenToAceCards();
-        if (checkCardInList(list, cardsToCheck)) {
-            return cardsToCheck;
+    public static Meld getInstance() {
+        if (instance == null) {
+            instance = new Meld();
         }
-        return null;
+        return instance;
     }
 
-    private List<String> checkAceRunExtraKing(List<Card> list) {
-        List<String> cardsToCheck = new ArrayList<>(getTenToAceCards());
-        cardsToCheck.add(Rank.KING.getRankCardValue() + Pinochle.trumpSuit);
-        if (checkCardInList(list, cardsToCheck)) {
-            return cardsToCheck;
-        }
-        return null;
+    private void populateMeld() {
+        // Create all the melds
+
+        // Basic meld.
+        MeldDecorator base = new MeldDecorator(new BasicMeld());
+
+        // Ace Run based melds.
+        MeldDecorator aceRun = new AceRunDecorator(new BasicMeld());
+        MeldDecorator aceRunK = new KingDecorator(new AceRunDecorator(new BasicMeld()));
+        MeldDecorator aceRunQ = new QueenDecorator(new AceRunDecorator(new BasicMeld()));
+        MeldDecorator aceRunRM = new KingDecorator(new QueenDecorator(new AceRunDecorator(new BasicMeld())));
+        MeldDecorator doubleAR = new DoubleDecorator(new AceRunDecorator(new BasicMeld()));
+
+        // Marriage based melds.
+
+
+        // Pinochle based melds.
+        MeldDecorator pinochle = new PinochleDecorator(new BasicMeld());
+        MeldDecorator doubleP = new DoubleDecorator(new PinochleDecorator(new BasicMeld()));
+
+        // Jacks Abound meld.
+        MeldDecorator jacksAbound = new JacksAboundDecorator(new BasicMeld());
+
+        // Aces Around meld.
+        MeldDecorator acesAround = new AcesAroundDecorator(new BasicMeld());
+
+        // Dix meld.
+        MeldDecorator dix = new DixDecorator(new BasicMeld());
+
+
+        // Add melds to list in the correct order based on score in decreasing order.
+        assert melds != null;
+        this.melds.addAll(Arrays.asList(
+                doubleAR, jacksAbound, doubleP, aceRunRM, aceRunK, aceRunQ, aceRun, acesAround
+                /*Missing Royal marriage, pinochle, common marriage, dix.*/
+        ));
     }
 
-    private List<String> checkAceRunExtraQueen(List<Card> list) {
-        List<String> cardsToCheck = new ArrayList<>(getTenToAceCards());
-        cardsToCheck.add(Rank.QUEEN.getRankCardValue() + Pinochle.trumpSuit);
-
-        if (checkCardInList(list, cardsToCheck)) {
-            return cardsToCheck;
-        }
-        return null;
+    public List<MeldDecorator> getMelds() {
+        return melds;
     }
 
-    private List<String> checkRoyalMarriage(List<Card> list) {
-        List<String> cardsToCheck = Arrays.asList(
-                Rank.QUEEN.getRankCardValue() + Pinochle.trumpSuit,
-                Rank.KING.getRankCardValue() + Pinochle.trumpSuit);
-        if (checkCardInList(list, cardsToCheck)) {
-            return cardsToCheck;
-        }
-        return null;
-    }
-
-    private int calculateMeldingScore(List<Card> list) {
-        int score = 0;
-        List<String> cardsToRemove = checkAceRunExtraKing(list);
-        if (cardsToRemove != null) {
-            score += 190;
-            list = removeCardFromList(list, cardsToRemove);
-        }
-        cardsToRemove = checkAceRunExtraQueen(list);
-        if (cardsToRemove != null) {
-            score += 190;
-            list = removeCardFromList(list, cardsToRemove);
-        }
-
-        cardsToRemove = checkTenToAceRun(list);
-        if (cardsToRemove != null) {
-            score += 150;
-            list = removeCardFromList(list, cardsToRemove);
-        }
-
-        cardsToRemove = checkRoyalMarriage(list);
-        if (cardsToRemove != null) {
-            score += 40;
-            list = removeCardFromList(list, cardsToRemove);
-        }
-        return score;
-    }
 }
---
+++ app/src/main/java/Scoring.java
@@ -0,0 +1,41 @@
+public class Scoring {
+    /*
+    private List<String> checkTenToAceRun(List<Card> list) {
+        List<String> cardsToCheck = getTenToAceCards();
+        if (checkCardInList(list, cardsToCheck)) {
+            return cardsToCheck;
+        }
+        return null;
+    }
+
+
+    private int calculateMeldingScore(List<Card> list) {
+        int score = 0;
+        List<String> cardsToRemove = checkAceRunExtraKing(list);
+        if (cardsToRemove != null) {
+            score += 190;
+            list = removeCardFromList(list, cardsToRemove);
+        }
+        cardsToRemove = checkAceRunExtraQueen(list);
+        if (cardsToRemove != null) {
+            score += 190;
+            list = removeCardFromList(list, cardsToRemove);
+        }
+
+        cardsToRemove = checkTenToAceRun(list);
+        if (cardsToRemove != null) {
+            score += 150;
+            list = removeCardFromList(list, cardsToRemove);
+        }
+
+        cardsToRemove = checkRoyalMarriage(list);
+        if (cardsToRemove != null) {
+            score += 40;
+            list = removeCardFromList(list, cardsToRemove);
+        }
+        return score;
+    }
+
+ */
+
+}
